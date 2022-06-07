534c98f66f43e084c1950f9313299be296287ebf
Alex <alexsandrolima991@hotmail.com>
2022-06-07 16:08:37 -0300

    correção lint

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
-const inputEmail = document.querySelector('#input-email');
+const inputEmail = document.querySelector('#input-email-login');
 const inputPassword = document.querySelector('#input-password');
 const loginSubmit = document.querySelector('#login-btn');
 
-loginSubmit.addEventListener('click', function loginCheck(event){
-    
-    if(inputEmail.value === 'tryber@teste.com' && inputPassword.value === '123456'){
-        return alert('Olá, Tryber');
-    }
-    event.preventDefault();
-    alert('Email ou senha inválidos');
-})
+loginSubmit.addEventListener('click', function loginCheck(event) {
+  if (inputEmail.value === 'tryber@teste.com' && inputPassword.value === '123456') {
+    return alert('Olá, Tryber');
+  }
+  event.preventDefault();
+  return alert('Email ou senha inválidos');
+});
